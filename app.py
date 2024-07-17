99811f846302f32bd882f5d1e39ceca867ffde0f
Philos250 <jeandedh@andrew.cmu.edu>
2024-07-17 18:22:48 +0200

    Final running code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from flask import Flask,render_template,url_for,request
 
 from spacy_summarization import text_summarizer
-from gensim.summarization import summarize
+# from gensim.summarization import summarize
 from nltk_summarization import nltk_summarizer
 import time
 import spacy
